953c6f994478ccbb441dfadd41434d97f5c9296c
Yanko-Svitlana <jkjunsv@gmail.com>
2017-03-23 19:16:35 +0200

    Add header with styles

--- lesson22/wp-content/plugins/popular-post-widget/inc/pplr_post_script.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function popular_posts_scripts() {
-	wp_enqueue_style( "stylesheet", plugins_url( "style.css", __FILE__ ), FALSE );
-}
-
-add_action( 'wp_enqueue_scripts', 'popular_posts_scripts' );
---
+++ lesson23/wp-content/themes/blog-name/header.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Blog_Name
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'blog-name' ); ?></a>
+
+	<header id="masthead" class="site-header" role="banner">
+		<div class="container">
+			<div class="site-branding">
+				<?php
+				if ( is_front_page() && is_home() ) : ?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php else : ?>
+					<h2 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h2>
+					<?php
+				endif;
+
+
+				get_search_form();
+				?>
+			</div><!-- .site-branding -->
+			
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="fa fa-bars"></i></button>
+				<?php wp_nav_menu( array(
+					'theme_location' => 'main-menu',
+					'menu_id' 		 => 'primary-menu',
+					'menu_class'	 => 'brand-color',
+					'container'		 => '',
+				) ); ?>
+			</nav><!-- #site-navigation -->
+		</div>
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
